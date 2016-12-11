3e401e758ac2b392fb7c9e5f536e845ccbd76577
Ryan Thames <ryanthames@gmail.com>
2016-12-11 09:52:47 -0600

    Create req/optional params in order; Change user changeset to default params to empty map instead of empty atom

--- apps/bss_web/web/models/order.ex
+++ apps/bss_web/web/models/order.ex
@@ -1,6 +1,9 @@
 defmodule BssWeb.Order do
   use BssWeb.Web, :model
 
+  @required_fields ~w(first_name last_name address_line_1 city state zip email)
+  @optional_fields ~w(address_line_2)
+
   schema "orders" do
     field :first_name, :string
     field :last_name, :string
@@ -14,13 +17,9 @@ defmodule BssWeb.Order do
 
     timestamps()
   end
-
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
+  
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:first_name, :last_name, :address_line_1, :address_line_2, :city, :state, :zip, :email, :status])
-    |> validate_required([:first_name, :last_name, :address_line_1, :address_line_2, :city, :state, :zip, :email, :status])
+    |> cast(params, @required_fields, @optional_fields)
   end
 end
