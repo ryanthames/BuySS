eff094aa940fa5a1d8b17d6fdf16dce2862c4780
Ryan Thames <ryanthames@gmail.com>
2016-12-16 12:26:56 -0600

    Add test helpers for inserting users and orders

---
+++ apps/bss_web/test/support/test_helpers.ex
@@ -0,0 +1,22 @@
+defmodule BssWeb.TestHelpers do
+  alias BssWeb.Repo
+
+  def insert_user(attrs \\ %{}) do
+    changes = Dict.merge(%{
+      name: "Some User",
+      username: "user#{Base.encode16(:crypto.rand_bytes(8))}",
+      password: "supersecret",
+      admin: false
+    }, attrs)
+
+    %BssWeb.User{}
+    |> BssWeb.User.registration_changeset(changes)
+    |> Repo.insert!()
+  end
+
+  def insert_order(attrs \\ %{}) do
+    %BssWeb.Order{}
+    |> BssWeb.Order.changeset(attrs)
+    |> Repo.insert!()
+  end
+end
