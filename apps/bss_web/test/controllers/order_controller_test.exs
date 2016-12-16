5bfe19b1c844fc035cce35dd4393ad1f0c4ff04f
Ryan Thames <ryanthames@gmail.com>
2016-12-16 16:26:17 -0600

    Add login_as tag to order controllers

--- apps/bss_web/test/controllers/order_controller_test.exs
+++ apps/bss_web/test/controllers/order_controller_test.exs
@@ -1,6 +1,16 @@
 defmodule BssWeb.OrderControllerTest do
   use BssWeb.ConnCase
 
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(build_conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
   test "requires user authentication on all non-create actions", %{conn: conn} do
     Enum.each([
       # new and create are available to the public...
@@ -13,5 +23,33 @@ defmodule BssWeb.OrderControllerTest do
       assert html_response(conn, 302)
       assert conn.halted
     end)
+  end
+
+  @tag login_as: "max"
+  test "list all orders on index", %{conn: conn} do
+    some_order = insert_order(%{
+      first_name: "Bob",
+      last_name: "Johnson",
+      address_line_1: "123 Whatever Dr",
+      city: "Whatever",
+      state: "NY",
+      zip: 12345,
+      email: "bob.johnson@whatever.com"
+    })
+
+    another_order = insert_order(%{
+      first_name: "Bill",
+      last_name: "Smith",
+      address_line_1: "543 Whatever Dr",
+      city: "Whatever",
+      state: "NY",
+      zip: 12345,
+      email: "bill.smith@whatever.com"
+    })
+
+    conn = get conn, order_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing orders/
+    assert String.contains?(conn.resp_body, some_order.address_line_1)
+    assert String.contains?(conn.resp_body, another_order.address_line_1)
   end
 end
