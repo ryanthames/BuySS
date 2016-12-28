fd507b3889cf988ad8275bfcf77b3898d741a9c6
Ryan Thames <ryanthames@gmail.com>
2016-12-27 20:08:14 -0600

    Finish order integration tests

--- apps/bss_web/test/controllers/order_controller_test.exs
+++ apps/bss_web/test/controllers/order_controller_test.exs
@@ -73,6 +73,46 @@ defmodule BssWeb.OrderControllerTest do
   # TODO test creating order with errors
 
   # TODO other CRUD actions
+  @tag login_as: "max"
+  test "deletes order", %{conn: conn} do
+    count_before = order_count(Order)
+    some_order = insert_order(@valid_attrs)
+
+    assert order_count(Order) == count_before + 1
+
+    conn = delete(conn, order_path(conn, :delete, some_order.id))
+
+    assert html_response(conn, 302)
+
+    assert order_count(Order) == count_before
+
+    refute Repo.get(Order, some_order.id)
+  end
+
+  @tag login_as: "max"
+    test "edits shows order edit page", %{conn: conn} do
+      order = insert_order(@valid_attrs)
+      conn = get(conn, order_path(conn, :edit, order))
+      assert html_response(conn, :ok) =~ ~r/Edit order.*#{order.email}/s
+    end
+
+  @tag login_as: "max"
+  test "updates order and redirects", %{conn: conn} do
+    some_order = insert_order(@valid_attrs)
+    new_attrs = Map.put(@valid_attrs, :email, "test@whatever.com")
+
+    conn = put(conn, order_path(conn, :update, some_order), order: new_attrs)
+    assert html_response(conn, 302)
+    assert Repo.get(Order, some_order.id).email == "test@whatever.com"
+  end
+
+  @tag login_as: "max"
+    test "does not update invalid order", %{conn: conn} do
+      order = insert_order(@valid_attrs)
+      conn = put(conn, order_path(conn, :update, order), order: %{email: ""})
+      assert html_response(conn, 200) =~ "check the errors"
+      assert Repo.get(Order, order.id).email == order.email
+    end
 
   defp order_count(query), do: Repo.one(from o in query, select: count(o.id))
 end
